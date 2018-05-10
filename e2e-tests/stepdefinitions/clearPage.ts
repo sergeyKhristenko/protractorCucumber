b7899f01eaa40681c3dd74ed59a7e33122c77199
sergeyKhristenko <sergey.khristenko@gmail.com>
2018-05-10 13:48:49 +0300

    test

--- e2e-tests/stepdefinitions/clearPage.ts
+++ e2e-tests/stepdefinitions/clearPage.ts
@@ -2,7 +2,7 @@ import { SearchPageObject } from '../pages/searchPage';
 const { Then } = require('cucumber');
 
 const search: SearchPageObject = new SearchPageObject();
-
+ 
 Then(/^I clear the search text$/, async () => {
   await search.searchTextBox.clear();
 });
